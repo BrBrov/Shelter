e20661dc9c72a6b3a3fb3aebefeb81354699d704
BrBrov <borbrov@gmail.com>
2026-06-27 11:23:04 +0300

    fix: fix closing menu from closes area

--- assets/scripts/burger-menu.js
+++ assets/scripts/burger-menu.js
@@ -1,23 +1,40 @@
 function menuHandler() {
-  const menu = document.querySelector('.header__navigation');
-  const button = document.querySelector('.header__btn-menu');
-
-  
-  if (menu.classList.contains('header__navigation_open')) {
-    menu.classList.remove('header__navigation_open');
-    button.classList.remove('header__btn-menu_open');
-
-    menu.removeEventListener('click', menuHandler);
-
-    scrollHandler(false);
+  if (document.querySelector('.header__navigation').classList.contains('header__navigation_open')) {
+    closeMenu();
   } else {
-    menu.classList.add('header__navigation_open');
-    button.classList.add('header__btn-menu_open');
-    
-    menu.addEventListener('click', menuHandler);
-
-    scrollHandler(true);
+    openMenu();
   }
+}
+
+function closeMenuHandler(event) {
+  const { target } = event;
+
+  if (target.className.match(new RegExp('header__navigation*', 'gi')) || target.className.match(new RegExp('header__btn-menu*', 'gi'))) {
+    return;
+  }
+
+  event.stopPropagation();
+  event.preventDefault();
+  closeMenu();
+}
+
+function closeMenu() {
+  document.querySelector('.header__navigation').classList.remove('header__navigation_open');
+  document.querySelector('.header__btn-menu').classList.remove('header__btn-menu_open');
+
+  document.querySelector('.header__navigation').removeEventListener('click', menuHandler);
+  document.removeEventListener('click', closeMenuHandler);
+
+  scrollHandler(false);
+}
+
+function openMenu() {
+  document.querySelector('.header__navigation').classList.add('header__navigation_open');
+  document.querySelector('.header__btn-menu').classList.add('header__btn-menu_open');
+
+  document.querySelector('.header__navigation').addEventListener('click', menuHandler);
+  document.addEventListener('click', closeMenuHandler);
+  scrollHandler(true);
 }
 
 function scrollHandler(mode) {
